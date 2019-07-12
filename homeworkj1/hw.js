c26e634a3c5770b2170aa8c911a8967488eaf13f
Volodymyr Gorbunov <deftime@gmail.com>
2019-07-12 17:02:41 +0300

    HW1 ready

---
+++ homeworkj1/hw.js
@@ -0,0 +1,88 @@
+// var pg = document.getElementById('block');
+// var rez;
+//
+// var qu = prompt('What is name of JS standart?', '');
+//
+// if (qu !== null) {
+//   if (qu == "ECMA") {
+//     rez = "Yes! ECMAScript.";
+//   } else {
+//     rez = "Don't you know?? ECMAScript!";
+//   }
+// } else {
+//   rez = "Why don't you want to ask?";
+// }
+//
+// pg.innerHTML = rez;
+
+// ------------------------------------------
+
+// var pg = document.getElementById('block');
+// var rez;
+//
+// var qu = +prompt('Enter number:', '');
+//
+// if (qu > 0) {
+//   rez = "It's 1";
+// } else if (qu < 0) {
+//   rez = "It's -1";
+// } else {
+//   rez = "And it is ZERO!";
+// }
+//
+// pg.innerHTML = rez;
+
+// ----------------------------
+
+// var pg = document.getElementById('block');
+// var rez;
+//
+// var qu1 = prompt('Enter login:', '');
+//
+// if (qu1 !== null) {
+//   if (qu1 == 'admin') {
+//     var qu2 = prompt('Enter password:', '');
+//     if (qu2 == 'blacklord') {
+//       rez = 'Enter ACCEPTED';
+//     } else if (qu2 == null) {
+//       rez = 'Enter reset';
+//     } else {
+//       rez = 'Password WRONG!';
+//     }
+//   } else {
+//     rez = "User not found";
+//   }
+// } else {
+//   rez = "Enter canceled";
+// }
+//
+// pg.innerHTML = rez;
+
+// -------------------------------------
+
+// var pg = document.getElementById('block');
+// var rez = "";
+//
+// for (var i = 2; i <= 10; i++) {
+//   if (i % 2 /*== 0*/) {
+//     continue;
+//   }
+//   rez += `${i}, `;
+// }
+//
+// pg.innerHTML = rez;
+
+// ----------------------------------------
+
+// var pg = document.getElementById('block');
+// var rez;
+//
+// var qu = +prompt('Enter number under 100', '');
+//
+// while (qu <= 100) {
+//   qu = +prompt('UNDER 100! English, mazafaka, do you speak it?', '');
+// }
+//
+// pg.innerHTML = "Ok, " + qu + " is under 100";
+
+// -----------------------------------------
