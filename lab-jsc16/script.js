c146d2a8f3e8634234b783b7ec5f1ab843c7caae
Volodymyr Gorbunov <deftime@gmail.com>
2019-09-03 15:09:43 +0300

    Lab17, Lab16(v2), HW10 not-ready

--- lab-jsc16/script.js
+++ lab-jsc16/script.js
@@ -6,8 +6,9 @@ butt.addEventListener('click', reqFun);
 
 function reqFun() {
   var xhr = new XMLHttpRequest();
-
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+  let url = 'https://jsonplaceholder.typicode.com/todos';
+  // let url = 'https://cdn.pixabay.com/photo/2018/10/30/16/06/water-lily-3784022__340.jpg';
+  xhr.open('GET', url);
 
   xhr.send();
 
@@ -15,11 +16,20 @@ function reqFun() {
 
   xhr.onload = function() {
     // rez.innerHTML = '';
-    resp = JSON.parse(xhr.response);
+    try {
+      resp = JSON.parse(xhr.response);
+      if (typeof(resp) != 'object') {
+        throw new Error('Fatal error!')
+      }
+    } catch (e) {
+      alert(e.name + ': ' + e.massage);
+    }
+
 
     for (let i = 0; i < resp.length; i++) {
       (function(j) {
         setTimeout(function() {
+          let {id, title, completed: checkBox} = resp[j];
           let cont = document.createElement('div');
           cont.classList.add('cont');
           let fLine = document.createElement('div');
@@ -29,11 +39,17 @@ function reqFun() {
           let chk = document.createElement('input');
           chk.setAttribute('type', 'checkbox');
           chk.disabled = true;
-          fLine.innerHTML = `User ID - ${resp[j].userId}`;
-          sLine.innerHTML = `${resp[j].title}`;
-          if (resp[j].completed == true) {
+
+          fLine.innerHTML = `User ID - ${id}`;
+          sLine.innerHTML = `${title}`;
+          if (checkBox == true) {
             chk.checked = true;
           }
+          // fLine.innerHTML = `User ID - ${resp[j].userId}`;
+          // sLine.innerHTML = `${resp[j].title}`;
+          // if (resp[j].completed == true) {
+          //   chk.checked = true;
+          // }
           cont.append(fLine);
           cont.append(sLine);
           cont.append(chk);
---
+++ lab-jsc17/script.js
@@ -0,0 +1,7 @@
+var obj = {
+  title: 'test',
+  width: 300,
+  height: '25'
+}
+
+let {title: objTit = 25, width: objWi, height: objHi} = obj;
