713890e5316a12aecdc8ecf39f72745b3ab73e71
Volodymyr Gorbunov <deftime@gmail.com>
2019-10-07 17:52:29 +0300

    HW10 letter counter

--- homeworkj10/script.js
+++ homeworkj10/script.js
@@ -1,3 +1,25 @@
-function getUsers(...arr) {
-  
+document.querySelector('#butt').addEventListener('click', function() {
+  let rez = wordStat(document.querySelector('input').value);
+
+  document.querySelector('.block').innerHTML = '';
+
+  for (let key in rez) {
+    document.querySelector('.block').innerHTML += `${key}: ${rez[key]}<br>`;
+  }
+})
+
+
+function wordStat(str) {
+  let rezObj = {};
+  let arrStr = str.split('');
+  for (let i = 0; i < arrStr.length; i++) {
+    let count = 0;
+    for (var j = 0; j < arrStr.length; j++) {
+      if (arrStr[i] == arrStr[j]) {
+        count++;
+      }
+    }
+    rezObj[arrStr[i]] = count;
+  }
+  return rezObj;
 }
